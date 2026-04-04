533ebd8b615c49b4ebb9cd501b55907d32d308c6
LifeTalk Ministry <yourgithubemail@example.com>
2026-04-05 04:52:40 +0800

    build to docs for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: "/clara-app-test/",
+  build: {
+    outDir: "docs",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
