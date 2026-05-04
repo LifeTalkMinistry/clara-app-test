3641b036ed59ee1c6049100dec58dcbeab6da02b
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-05 06:59:04 +0800

    Create dashboard money section

---
+++ src/pages/fresh/dashboard/sections/DashboardMoneySection.jsx
@@ -0,0 +1,5 @@
+import MoneyLeftUi from "@/components/fresh/main-dashboard/moneysummary/MoneyLeftUi.jsx";
+
+export default function DashboardMoneySection(props) {
+  return <MoneyLeftUi {...props} />;
+}
