6dd803fb4b7da640211c00518050c0f50a613827
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-05 22:56:41 +0800

    Create Learning Hub wrapper

---
+++ src/components/fresh/main-dashboard/learning-hub/LearningHub.jsx
@@ -0,0 +1,25 @@
+import useLearningHub from "./logic/useLearningHub";
+import LearningHubCarousel from "./ui/LearningHubCarousel";
+import LearningMaterialModal from "./modal/LearningMaterialModal";
+
+export default function LearningHub() {
+  const {
+    materials,
+    selectedMaterial,
+    isOpen,
+    openMaterial,
+    closeMaterial,
+  } = useLearningHub();
+
+  return (
+    <section className="w-full">
+      <LearningHubCarousel materials={materials} onOpenMaterial={openMaterial} />
+
+      <LearningMaterialModal
+        isOpen={isOpen}
+        material={selectedMaterial}
+        onClose={closeMaterial}
+      />
+    </section>
+  );
+}
