be2703abcae18ee1a9e81baded0d025fe426fbe5
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-05 08:41:56 +0800

    Wire billboard hook into fresh dashboard

--- src/pages/fresh/dashboard/Dashboard.jsx
+++ src/pages/fresh/dashboard/Dashboard.jsx
@@ -2,15 +2,20 @@ import DashboardBillboardSection from "./sections/DashboardBillboardSection";
 import DashboardCarouselSection from "./sections/DashboardCarouselSection";
 import DashboardMoneySection from "./sections/DashboardMoneySection";
 import useDashboardUIState from "./hooks/useDashboardUIState";
+import useDashboardBillboard from "./hooks/useDashboardBillboard";
 
 export default function Dashboard() {
   const { showBillboard } = useDashboardUIState();
+
+  const billboardProps = useDashboardBillboard({
+    show: showBillboard,
+  });
 
   return (
     <div className="px-4 pt-4 space-y-4">
 
       {/* BILLBOARD */}
-      <DashboardBillboardSection show={showBillboard} />
+      <DashboardBillboardSection {...billboardProps} />
 
       {/* CAROUSEL */}
       <div>
