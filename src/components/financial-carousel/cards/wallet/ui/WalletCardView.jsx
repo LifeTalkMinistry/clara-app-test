0e07707e06a24676db461278dc9c861d45cb31fc
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-06 09:06:50 +0800

    Extract wallet carousel card view

---
+++ src/components/financial-carousel/cards/wallet/ui/WalletCardView.jsx
@@ -0,0 +1,35 @@
+import WalletCard from "@/components/WalletCard";
+
+export default function WalletCardView({
+  data = {},
+  selectedDashboardTheme,
+  expandedFinanceCard,
+  toggleFinanceDetails,
+  financeActionLoading,
+  onCreateWallet,
+  onMoveWallet,
+  onDeleteWallet,
+  onAddMoney,
+  onTransferMoney,
+  onEditWallet,
+}) {
+  return (
+    <div className="h-full min-h-[inherit] flex flex-col">
+      <WalletCard
+        wallets={data.wallets}
+        walletMoney={data.walletMoney}
+        walletPreviewTransactions={data.walletPreviewTransactions}
+        theme={selectedDashboardTheme}
+        expanded={expandedFinanceCard === "wallets"}
+        onToggleDetails={() => toggleFinanceDetails?.("wallets")}
+        financeActionLoading={financeActionLoading}
+        onCreateWallet={onCreateWallet}
+        onMoveWallet={onMoveWallet}
+        onDeleteWallet={onDeleteWallet}
+        onAddMoney={onAddMoney}
+        onTransferMoney={onTransferMoney}
+        onEditWallet={onEditWallet}
+      />
+    </div>
+  );
+}
