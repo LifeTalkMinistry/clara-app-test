8e5bd939494dddcb250d75294fd9d1ca678da7be
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-28 10:54:05 +0800

    Add schedule full page wrapper placeholder

---
+++ src/components/fresh/main-dashboard/dashboard-panels/schedule/DashboardSchedulePanelFullPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import OriginalDashboardSchedulePanel from "./DashboardSchedulePanel.jsx";
+
+export default function DashboardSchedulePanelFullPage() {
+  return React.createElement(OriginalDashboardSchedulePanel);
+}
