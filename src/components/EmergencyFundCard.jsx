50191d79a571325d93598621a8e8af90c4326f3d
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-05-30 18:53:03 +0800

    Use synced emergency storage card

--- src/components/EmergencyFundCard.jsx
+++ src/components/EmergencyFundCard.jsx
@@ -1,1 +1,1 @@
-export { default } from "@/components/fresh/main-dashboard/carousel/EmergencyFundCardStorageWallet";
+export { default } from "@/components/fresh/main-dashboard/carousel/EmergencyFundCardStorageWalletSynced";
