590956ac8272cc7f91503d90ac5ada286b22cf03
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-06-16 02:48:06 +0800

    Delete obsolete profile page

--- src/pages/Profile.jsx
+++
@@ -1,7 +0,0 @@
-export default function Profile() {
-  return (
-    <div className="theme-page-shell min-h-screen px-4 py-10 text-center text-white">
-      <p className="text-sm text-white/60">This old profile screen has been removed.</p>
-    </div>
-  );
-}
