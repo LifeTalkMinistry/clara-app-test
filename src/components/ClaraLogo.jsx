75747260520cb4b8e00a6a6d91f30ceedf5aca26
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-06-18 12:06:22 +0800

    Crop inner logo layer inside Clara container

--- src/components/ClaraLogo.jsx
+++ src/components/ClaraLogo.jsx
@@ -37,7 +37,7 @@ export default function ClaraLogo({
           <img
             src={logo}
             alt="CLARA Logo"
-            className="h-full w-full origin-center scale-[1.25] object-contain p-0 drop-shadow-[0_0_16px_rgba(45,212,191,0.28)]"
+            className="relative z-10 h-full w-full origin-center scale-[1.65] object-cover p-0 drop-shadow-[0_0_16px_rgba(45,212,191,0.28)]"
           />
         </div>
       </div>
