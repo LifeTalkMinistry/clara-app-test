32d1212c0faddbdeadb6ce1469bcd1103c8c9b26
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-06-20 00:05:20 +0800

    refactor: define orb guide phases

---
+++ src/components/fresh/main-dashboard/guide/orbGuideState.js
@@ -0,0 +1,10 @@
+export const ORB_GUIDE_PHASES = Object.freeze([
+  "intro",
+  "await-single",
+  "showing-log-expense",
+  "await-double",
+  "showing-transaction-hub",
+  "await-hold",
+  "showing-clara-chat",
+  "complete",
+]);
