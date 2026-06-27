b9e56f89d020e4bb7ca840a3c3509f41574c05a3
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-06-28 07:07:38 +0800

    Add haptic feedback to finance card toggles

--- src/lib/financeCardToggleSound.js
+++ src/lib/financeCardToggleSound.js
@@ -1,4 +1,5 @@
 import learningHubSoundUrl from "../../learning-hub.mp3.wav";
+import { triggerClaraHaptic } from "@/lib/claraHaptics";
 
 const CLARA_SOUND_STORAGE_KEY = "clara:sound-enabled";
 const CLARA_SOUND_VOLUME_KEY = "clara:sound-volume";
@@ -44,6 +45,7 @@ function getFinanceToggleAudio() {
 }
 
 export function playFinanceCardToggleSound() {
+  triggerClaraHaptic("light");
   if (!isSoundEnabled()) return false;
 
   const audio = getFinanceToggleAudio();
