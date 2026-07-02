e86789120661b02a2f9b33a6e4c55f819f634f1d
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-07-02 11:31:29 +0800

    Make Income Hub timing wrapper valid JavaScript

--- src/components/financial-carousel/cards/investment/ui/IncomeHubExpandedSurfaces.js
+++ src/components/financial-carousel/cards/investment/ui/IncomeHubExpandedSurfaces.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { formatIncomeTimingLabel } from "@/lib/recurringCashFlowRepository";
 import { IncomeSourcePreviewRow as BaseIncomeSourcePreviewRow } from "./IncomeHubExpandedSurfaces.jsx";
 
@@ -6,14 +7,19 @@ export * from "./IncomeHubExpandedSurfaces.jsx";
 export function IncomeSourcePreviewRow(props) {
   const timingLabel = formatIncomeTimingLabel(props.source);
 
-  return (
-    <div className="relative">
-      <BaseIncomeSourcePreviewRow {...props} />
-      {timingLabel ? (
-        <p className="pointer-events-none absolute left-[75px] right-12 top-[76px] truncate text-[9px] font-semibold leading-none text-cyan-100/56">
-          Usually received: {timingLabel}
-        </p>
-      ) : null}
-    </div>
+  return React.createElement(
+    "div",
+    { className: "relative" },
+    React.createElement(BaseIncomeSourcePreviewRow, props),
+    timingLabel
+      ? React.createElement(
+          "p",
+          {
+            className:
+              "pointer-events-none absolute left-[75px] right-12 top-[76px] truncate text-[9px] font-semibold leading-none text-cyan-100/56",
+          },
+          `Usually received: ${timingLabel}`
+        )
+      : null
   );
 }
