acdfaca81fe2134f6e11a4796239006f3d7726d5
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-08-08 08:59:37 +0800

    Show labeled Community back button in Messages

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,7 @@ import { installClaraGuideScheduleRuntime } from "./runtime/claraGuideScheduleRu
 import "./runtime/installClaraRuntimePatches";
 import "./runtime/installManualExpenseKeyboardGuard";
 import "./index.css";
+import "./messages-back-to-community-label.css";
 import "./manual-expense-wallet-step.css";
 import "./guide-mode-stacking.css";
 import "./guide-mode-finance-spotlight.css";
