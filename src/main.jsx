e721ab49675c9a44224a0800b637d03a1bee305b
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-08-09 03:11:46 +0800

    Remove Community header global size override

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,6 @@ import { installClaraGuideScheduleRuntime } from "./runtime/claraGuideScheduleRu
 import "./runtime/installClaraRuntimePatches";
 import "./runtime/installManualExpenseKeyboardGuard";
 import "./index.css";
-import "./community-header-action-size.css";
 import "./support-orb-position.css";
 import "./messages-back-to-community-label.css";
 import "./messages-inbox-refine.css";
