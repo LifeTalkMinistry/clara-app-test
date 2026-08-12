ac73fa0cc5473401880d0a422e8fdcbcb11e7d4e
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-08-12 13:52:27 +0800

    Load finance card containment fix

--- src/main.jsx
+++ src/main.jsx
@@ -52,6 +52,7 @@ import "./clara-home-daily-tip-official.css";
 import "./clara-no-bubble-effects.css";
 import "./clara-home-learning-rail-official.css";
 import "./clara-home-finance-card-system.css";
+import "./clara-home-finance-card-containment-fix.css";
 import "./clara-home-money-left-official.css";
 import "./clara-universal-background.css";
 import "./clara-home-responsive-spacing.css";
