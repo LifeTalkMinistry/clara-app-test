b81bdc57b0ae79840b9910b325307aa448c10a6e
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-08-14 11:43:16 +0800

    Use manual scheduler in full-page view

--- src/components/fresh/main-dashboard/dashboard-panels/schedule/DashboardSchedulePanelFullPage.js
+++ src/components/fresh/main-dashboard/dashboard-panels/schedule/DashboardSchedulePanelFullPage.js
@@ -1,6 +1,1 @@
-import React from "react";
-import OriginalDashboardSchedulePanel from "./DashboardSchedulePanel.jsx";
-
-export default function DashboardSchedulePanelFullPage() {
-  return React.createElement(OriginalDashboardSchedulePanel);
-}
+export { default } from "./DashboardScheduleManualPanel.jsx";
