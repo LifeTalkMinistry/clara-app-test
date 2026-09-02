4241c8a594e3a3d5541893790de22518bf5deb84
LifeTalk Ministry <96472293+LifeTalkMinistry@users.noreply.github.com>
2026-09-02 21:49:43 +0800

    Load progressive Means breakdown runtime

--- src/main.jsx
+++ src/main.jsx
@@ -35,6 +35,7 @@ import "./runtime/installBudgetRecentReuse";
 import "./runtime/installBudgetDocumentationHeaderCleanup";
 import "./runtime/installQuickActionsBudgetSetupNoticeCleanup";
 import "./runtime/installProfileUnderConstructionModal";
+import "./runtime/installClaraProgressiveMeansBreakdown";
 import "./index.css";
 import "./support-orb-position.css";
 import "./messages-back-to-community-label.css";
